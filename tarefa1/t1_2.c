034b16c05773df1920a8c39d8d5db3bef75acbd6
Sueyvid <sueyvidr8@gmail.com>
2025-09-14 22:13:31 -0300

    t1: revised final solution

--- tarefa1/t1_2.c
+++ tarefa1/t1_2.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 
 void mul_matriz_vetor_coluna_externa(int **matriz, int *vetor, int *resultado, int linhas, int colunas) {
-    // preencher vetor resultado
     for (int i = 0; i < linhas; i++) {
         resultado[i] = 0;
     }
 
-    // multiplicar (varrendo colunas primeiro)
     for (int j = 0; j < colunas; j++) {
         for (int i = 0; i < linhas; i++) {
             resultado[i] += matriz[i][j] * vetor[j];
@@ -19,34 +17,27 @@ void mul_matriz_vetor_coluna_externa(int **matriz, int *vetor, int *resultado, i
 int main() {
     int n = 4096;
 
-    // Alocar matriz n x n
     int **M = (int **)malloc(n * sizeof(int *));
     for (int i = 0; i < n; i++) {
         M[i] = (int *)malloc(n * sizeof(int));
     }
 
-    // Preencher matriz
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             M[i][j] = 1;
         }
     }
 
-    // Alocar vetor
     int *V = (int *)malloc(n * sizeof(int));
 
-    // Preencher vetor
     for (int i = 0; i < n; i++) {
         V[i] = 1;
     }
 
-    // Alocar resultado
     int *resultado = (int *)malloc(n * sizeof(int));
 
-    // Multiplicação (MxV)
     mul_matriz_vetor_coluna_externa(M, V, resultado, n, n);
 
-    // Liberar memória
     for (int i = 0; i < n; i++) {
         free(M[i]);
     }
