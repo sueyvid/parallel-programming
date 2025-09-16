aef4c9f43c7fc4349450c79942b5c1361d9ae8ed
Sueyvid <sueyvidr8@gmail.com>
2025-09-16 09:56:53 -0300

    t10: initial solution

---
+++ tarefa10/t10_2.c
@@ -0,0 +1,32 @@
+// race_condition.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+#define N 1000000
+#define THREADS 4
+
+int main() {
+    int total_count = 0;
+
+    #pragma omp parallel num_threads(THREADS)
+    {
+        unsigned int seed = time(NULL) ^ omp_get_thread_num();
+        
+        #pragma omp for
+        for (int i = 0; i < N; i++) {
+            double x = rand_r(&seed) / (double)RAND_MAX;
+            double y = rand_r(&seed) / (double)RAND_MAX;
+            if (x * x + y * y <= 1) {
+                #pragma omp atomic
+                total_count++;
+            }
+        }
+    }
+
+    double pi = 4.0 * total_count / N;
+    printf("Pi = %f\n", pi);
+
+    return 0;
+}
