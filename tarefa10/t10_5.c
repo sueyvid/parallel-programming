f2b08afa8cb1200f24f2adc534d45b8479b240e9
Sueyvid <sueyvidr8@gmail.com>
2025-09-21 18:25:37 -0300

    t5: added version 5

--- tarefa10/t10_5.c
+++ tarefa10/t10_5.c
@@ -10,16 +10,15 @@
 int main() {
     int total_count = 0;
 
-    #pragma omp parallel reduction(+:total_count)
+    #pragma omp parallel
     {
         unsigned int seed = time(NULL) ^ omp_get_thread_num();
         
-        #pragma omp for
+        #pragma omp for reduction(+:total_count)
         for (int i = 0; i < N; i++) {
             double x = rand_r(&seed) / (double)RAND_MAX;
             double y = rand_r(&seed) / (double)RAND_MAX;
             if (x * x + y * y <= 1) {
-                #pragma omp critical
                 total_count++;
             }
         }
