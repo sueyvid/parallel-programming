a2c8898229e283813931236a450cc0c33c7a21d2
Sueyvid <sueyvidr8@gmail.com>
2025-10-02 17:46:04 -0300

    t11: remove print

--- tarefa11/t11_3d.c
+++ tarefa11/t11_3d.c
@@ -10,8 +10,8 @@
 
 int main() {
     // Parâmetros padrão
-    int Nx = 50, Ny = 50, Nz = 50; // grade 3D
-    int steps = 50;                // número de passos no tempo
+    int Nx = 100, Ny = 100, Nz = 100; // grade 3D
+    int steps = 1000;                // número de passos no tempo
     double nu = 0.01;              // viscosidade
     double dt = 0.0005;            // passo no tempo
     double h = 1.0 / Nx;
@@ -25,18 +25,18 @@ int main() {
     int ic = Nx/2, jc = Ny/2, kc = Nz/2;
     w[IDX(ic,jc,kc,Nx,Ny)] = 1.0;
 
-    // Arquivo CSV para saída
-    FILE *fp = fopen("output.csv", "w");
-    if (!fp) {
-        perror("Erro ao abrir arquivo");
-        return 1;
-    }
-    fprintf(fp, "step,i,j,k,value\n");
+    // // Arquivo CSV para saída
+    // FILE *fp = fopen("output.csv", "w");
+    // if (!fp) {
+    //     perror("Erro ao abrir arquivo");
+    //     return 1;
+    // }
+    // fprintf(fp, "step,i,j,k,value\n");
 
-    double start = omp_get_wtime();
+    // double start = omp_get_wtime();
 
     for (int n = 0; n < steps; n++) {
-#pragma omp parallel for collapse(3) schedule(static)
+#pragma omp parallel for collapse(3) schedule(guided, 20)
         for (int k = 0; k < Nz; k++) {
             for (int j = 0; j < Ny; j++) {
                 for (int i = 0; i < Nx; i++) {
@@ -62,23 +62,23 @@ int main() {
         // Swap
         double *tmp = w; w = w_new; w_new = tmp;
 
-        // Salva estado a cada X passos (ex: a cada 10)
-        if (n % 10 == 0) {
-            for (int k = 0; k < Nz; k++) {
-                for (int j = 0; j < Ny; j++) {
-                    for (int i = 0; i < Nx; i++) {
-                        fprintf(fp, "%d,%d,%d,%d,%f\n", n, i, j, k, w[IDX(i,j,k,Nx,Ny)]);
-                    }
-                }
-            }
-        }
+        // // Salva estado a cada X passos (ex: a cada 10)
+        // if (n % 10 == 0) {
+        //     for (int k = 0; k < Nz; k++) {
+        //         for (int j = 0; j < Ny; j++) {
+        //             for (int i = 0; i < Nx; i++) {
+        //                 fprintf(fp, "%d,%d,%d,%d,%f\n", n, i, j, k, w[IDX(i,j,k,Nx,Ny)]);
+        //             }
+        //         }
+        //     }
+        // }
     }
 
-    double end = omp_get_wtime();
-    printf("Simulação finalizada em %f segundos\n", end - start);
-    printf("Valor no centro = %f\n", w[IDX(ic,jc,kc,Nx,Ny)]);
+    // double end = omp_get_wtime();
+    // printf("Simulação finalizada em %f segundos\n", end - start);
+    // printf("Valor no centro = %f\n", w[IDX(ic,jc,kc,Nx,Ny)]);
 
-    fclose(fp);
+    // fclose(fp);
     free(w); free(w_new);
 
     return 0;
